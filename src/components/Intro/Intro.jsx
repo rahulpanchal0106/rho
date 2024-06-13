330f0926981427748a19e2047863e7cb4947794a
Rahul Panchal <rm5901960@gmail.com>
2024-06-13 11:28:36 +0530

    font size

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -55,7 +55,7 @@ function Intro(){
 
                 alignItems: isMobile?"": "center"
             }}>I am <strong style={{
-                fontSize:"75px",
+                fontSize:"65px",
                 color:"#00ADB5",
                 padding:"0px",
                 marginLeft: isMobile?"0px":'7px'
